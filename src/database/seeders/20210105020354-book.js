47c3832b322db08564318ec29e0a630e801e8571
pc <bookingonl>
2021-05-08 21:24:41 +0700

    abc

--- src/database/seeders/20210105020354-book.js
+++ src/database/seeders/20210105020354-book.js
@@ -19,12 +19,7 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
 
   }
 };
